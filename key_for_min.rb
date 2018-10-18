704356c2bc9b7a4428994379492c08f62a8ab809
Christian V <viray.christianj@gmail.com>
2018-10-18 19:22:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,15 +13,3 @@ end
   min_key
 end 
 
-
-def key_for_min_value(hash)
-  min_num = nil
-  min_key = nil
-  hash.each do |key, num|
-    if min_num == nil || num < min_num
-      min_key = key
-      min_num = num
-    end
-  end
-  min_key
-end
